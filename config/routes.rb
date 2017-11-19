3a741243b3c721b8d927c83480e421ca0a87b97d
Karlie Romas <karlie.romas@gmail.com>
2017-11-19 08:44:46 -0600

    add show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
+  # get '/posts', to: 'posts#index', as: 'posts'
+  # # index, show, edit, new, update, delete, create
 
-  get '/posts', to: 'posts#index'
-  # index, show, edit, new, update, delete, create
+  # get '/posts/:id', to: 'posts#show', as: 'post'
 end
