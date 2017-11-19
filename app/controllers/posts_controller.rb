f3cea7b3a95de5bb5bfbb8a23ee9bdbbf3051696
Karlie Romas <karlie.romas@gmail.com>
2017-11-19 09:31:53 -0600

    create/update/destroy posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,48 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
+    else
+      render :show
+    end
+
+  end
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
